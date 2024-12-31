205c4876c587bfdec1e4dc52cfb542045d9a9dbb
Abdullah <abdullah262001@gmail.com>
2024-12-31 13:03:55 +0530

    resolving issue

--- qtify/src/component/Logo/Logo.jsx
+++ qtify/src/component/Logo/Logo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import LogoImage from "../../assets/Logo.png";
-
+import styles from "./Logo.module.css"
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} height={34} sx={{marginBottom:10, marginTop:10}} />;
+  return <div className="styles.logoDiv">
+    <img src={LogoImage} alt="logo" width={67} />
+  </div>;
 }
