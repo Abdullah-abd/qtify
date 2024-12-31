0e5c3e095c33bbeb4f3b942025914359770dfb7e
Abdullah <abdullah262001@gmail.com>
2025-01-01 00:11:34 +0530

    tab scrollable

--- qtify/src/component/BasicTabs/BasicTabs.jsx
+++ qtify/src/component/BasicTabs/BasicTabs.jsx
@@ -1,22 +1,19 @@
-import * as React from 'react';
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-
-
-
+import * as React from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
-export default function BasicTabs({ handleChangeIndex}) {
+export default function BasicTabs({ handleChangeIndex }) {
   const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
@@ -25,12 +22,19 @@ export default function BasicTabs({ handleChangeIndex}) {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" 
-         TabIndicatorProps={{
-            style: { backgroundColor: "#34c94b" }
-        }} textColor='#34c94b' >
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          variant="scrollable"
+          scrollButtons="auto"
+          TabIndicatorProps={{
+            style: { backgroundColor: "#34c94b" },
+          }}
+          textColor="#34c94b"
+        >
           <Tab label="All" {...a11yProps(0)} />
           <Tab label="Rock" {...a11yProps(1)} />
           <Tab label="Pop" {...a11yProps(2)} />
@@ -41,4 +45,3 @@ export default function BasicTabs({ handleChangeIndex}) {
     </Box>
   );
 }
-
